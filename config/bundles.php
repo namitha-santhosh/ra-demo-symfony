2c02532ba65e4c3a42de4ba26d0bc6d4f4295355
Namitha <namitha.santhosh@people10.com>
2025-01-29 15:24:53 +0530

    login api fix

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,6 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
